2e4aa176a67dce412bd3d42b79281f35e5990402
Manish Singh <76744029+manish511-github@users.noreply.github.com>
2026-02-28 18:58:05 +0530

    Time: 0 ms (100%), Space: 25.9 MB (78.68%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     int trap(vector<int>& arr) {
-        // Apprach 1 :
-        // we track the leftmax and rightmax height using array
-        // and then find sum of filled water possible by adding water block at each step
+        // Apprach 2 :
+        // we keep track of leftmax and right max
+        // and keep on adding the possible water block with help of 2 pointers
         int n=arr.size();
-        vector<int> leftmax(n,0);
-        vector<int> rightmax(n,0);
-        for (int i=0;i<n;i++){
-            leftmax[i]= (i==0)?arr[i]:max(arr[i],leftmax[i-1]);
-            rightmax[n-1-i]= (i==0)?arr[n-1-i]:max(arr[n-1-i],rightmax[n-i]);
-        }
+        int left=0;
+        int right=n-1;
+        int leftmax=arr[0];
         int sum=0;
-        for (int i=0;i<n;i++)
-        {
-            sum+=(min(leftmax[i],rightmax[i])-arr[i]);
+        int rightmax=arr[n-1];      
+        while(left<right){
+            leftmax=max(arr[left],leftmax);
+            rightmax=max(arr[right],rightmax);
+            if (leftmax<rightmax){
+                sum+=(leftmax-arr[left]);
+                left++;
+            }
+            else{
+                sum+=(rightmax-arr[right]);
+                right--;
+            }
+
         }
         return sum;
-        
     }
 };

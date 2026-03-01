21bc2d0828b89b471db8777af0f280f17ea74542
Manish Singh <76744029+manish511-github@users.noreply.github.com>
2026-03-01 14:52:51 +0530

    Time: 3 ms (29.71%), Space: 16.4 MB (41.19%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-int helper(int i,int j,vector<vector<int>>& arr,vector<vector<int>>& dp){
-    if (i==0 && j==0){
-        return arr[i][j];
-    }
-    if (dp[i][j]!=-1){
-        return dp[i][j];
-    }
-    int up=INT_MAX-200;
-    if (i-1>=0){
-        up=helper(i-1,j,arr,dp);
-    }
-    int right=INT_MAX-200;
-    if (j-1>=0){
-        right= helper(i,j-1,arr,dp);
-    }
-    return dp[i][j]=min(arr[i][j]+up,arr[i][j]+right);
-}
-    int minPathSum(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
+    int minPathSum(vector<vector<int>>& arr) {
+        int n=arr.size();
+        int m=arr[0].size();
         vector<vector<int>> dp(n,vector<int>(m,-1));
-        return helper(n-1,m-1,grid,dp);
+        for (int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
+                if (i==0 && j==0){
+                    dp[i][j]=arr[i][j];
+                }
+                else 
+                {
+                    int up=INT_MAX;
+                    int right=INT_MAX;
+                    if (i-1>=0){
+                        up=arr[i][j]+dp[i-1][j];
+                    }
+                    if (j-1>=0){
+                        right=arr[i][j]+dp[i][j-1];
+                    }
+                    dp[i][j]=min(up,right);
+                }
+            }
+        }
+        return dp[n-1][m-1];
         
     }
 };

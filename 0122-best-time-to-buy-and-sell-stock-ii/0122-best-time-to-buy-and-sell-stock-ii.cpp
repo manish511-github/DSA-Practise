fc749b9281e5bc2f69ddd116e017337e35f9af1f
Manish Singh <76744029+manish511-github@users.noreply.github.com>
2026-03-03 23:12:00 +0530

    Time: 11 ms (15.74%), Space: 27.7 MB (6.58%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
-
-    int maxProfit(vector<int>& arr) {
-  int n = arr.size();
-        vector<vector<int>> dp(n + 1, vector<int>(2, 0)); 
-
-        for (int i = n - 1; i >= 0; i--) { 
-            for (int j = 1; j >= 0; j--) { 
-                int profit = 0;
-                if (j) { 
-                    profit = max(-arr[i] + dp[i + 1][0], dp[i + 1][1]);
-                } else {
-                    profit = max(arr[i] + dp[i + 1][1], dp[i + 1][0]);
-                }
-                dp[i][j] = profit;
-            }
+    int helper(int index,int buy, vector<int>& arr,vector<vector<int>> &dp){
+        // buy
+        // sell
+        if (index>=arr.size()){
+            return 0;
         }
+        int val1=0;
+        int val2=0;
+        int profit=INT_MIN;
+        if (dp[index][buy]!=-1)
+        {
+            return dp[index][buy];
+        }
+        if (buy){
+            val1 =-arr[index]+helper(index+1,0,arr,dp);
+            val2 = helper(index+1,1,arr,dp); 
+            profit=max(val1,val2);
+        }
+        else {
+            val1=arr[index]+helper(index+1,1,arr,dp);
+            val2=helper(index+1,0,arr,dp);
+            profit=max(val1,val2);
+        }
+        return dp[index][buy]=profit;
 
-        return dp[0][1]; 
+
+    }
+    int maxProfit(vector<int>& arr) {
+        int n=arr.size();
+        
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        return helper(0,1,arr,dp);
         
     }
 };

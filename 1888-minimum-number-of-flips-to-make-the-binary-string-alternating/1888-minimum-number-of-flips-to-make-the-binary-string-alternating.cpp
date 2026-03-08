10de432b23b273e2ec7cf81881d16adf92847e9c
Manish Singh <76744029+manish511-github@users.noreply.github.com>
2026-03-08 19:07:08 +0530

    Time: 12 ms (77.82%), Space: 17.2 MB (60.9%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -1,30 +1,13 @@
 class Solution {
 public:
-    string helper(int len, char ch){
-        string s = "";
-        for(int i = 0; i < len; i++){
-            s.push_back(ch);
-            ch = (ch == '0') ? '1' : '0';  
-        }
-        return s;
-    }
-    int compare (string s1, string s2){
-        int count=0;
-        for (int i=0;i<s1.length();i++){
-            if (s1[i]!=s2[i]){
-                count++;
-            }
-        }
-        return count;
-    }
+
     int minFlips(string s) {
         int mini=INT_MAX;
         // there are no operation count for task1 
         int n=s.length();
         string str1;
         string str = s+s;
-        string p1 = helper(n,'0');
-        string p2 = helper(n,'1');
+
         int tail=0;
         int diff1=0;
         int diff2=0;

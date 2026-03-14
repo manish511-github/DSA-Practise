2262443ea6885d4afc0aea477390fd69662dc45c
Manish Singh <76744029+manish511-github@users.noreply.github.com>
2026-03-14 17:01:45 +0530

    Time: 0 ms (100%), Space: 60.3 MB (55.75%) - LeetHub

--- 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
@@ -24,28 +24,17 @@ public:
 // 1  2,3  4 5 6   7 8
 // 8 76 543  
     int maximumGroups(vector<int>& arr) {
-int cnt=0;
-    int sum=0;
-    int len=0;
-    int lastsum=0;
-    int prevlen=0;
-    sort(arr.begin(),arr.end());
-    // 3 5 6 7 10 12
-    // 3  
-    for (int i=0;i<arr.size();i++){
-        sum+=arr[i];
-        len++;
-        if (sum>lastsum && len>prevlen){
-            lastsum=sum;
-            prevlen=len;
-            len=0;
-            sum=0;
-            cnt++;
+        int n = arr.size();
+        int k = 0;
+        int need = 1;
+
+        while (n >= need) {
+            n -= need;
+            need++;
+            k++;
         }
-        
-    }
-    return cnt;
-        
+
+        return k;
 
     }
 };

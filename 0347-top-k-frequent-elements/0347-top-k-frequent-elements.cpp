63f45acb8e79b1bceaf25c18912fc779f2908af0
Manish Singh <76744029+manish511-github@users.noreply.github.com>
2026-04-03 21:00:00 +0530

    Time: 0 ms (100%), Space: 17.8 MB (84.02%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,7 +4,7 @@ public:
         //Approch 1:-
         unordered_map<int,int>mp;
         vector<int>ans;
-        priority_queue<pair<int,int>>pq;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
         for (int i=0;i<arr.size();i++){
             mp[arr[i]]++;
 
@@ -12,27 +12,17 @@ public:
         
         for (auto it : mp){
             pq.push({it.second,it.first});
+            if (pq.size()>k){
+                pq.pop();
+            }
         }
         
-        while(!pq.empty() && k>0){
+        while(!pq.empty()){
             auto t=pq.top();
             pq.pop();
             ans.push_back(t.second);
-            k--;
         }
         return ans;
-        // O(n) + O(m \log m) + O(k \log m)
-        // Overall = O(n + m log m)
-
-        // If all elements are unique (m = n):
-        // Worst-case = O(n log n)
-
-        //Space Complexity 
-        //	1.	unordered_map → stores m unique elements → O(m)
-        //  2.	priority_queue → stores m elements → O(m)
-        //  3.	ans → stores k elements → O(k)
-
-        //    So total: //O(m + k)
 
     }
 };

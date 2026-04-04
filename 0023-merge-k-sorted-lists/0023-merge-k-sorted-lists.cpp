39b87fbbd164d703f835849dee05079000c712ed
Manish Singh <76744029+manish511-github@users.noreply.github.com>
2026-04-04 11:32:51 +0530

    Time: 0 ms (100%), Space: 18.3 MB (81.53%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,30 +10,30 @@
  */
 class Solution {
 public:
-    ListNode* helper(ListNode* l1,ListNode* l2){
-        ListNode * curr= new ListNode (-1);
-        ListNode* dummy =curr;
-        while(l1!=NULL && l2!=NULL){
-            if (l1->val<l2->val){
-                curr->next=l1;
-                l1=l1->next;
-                curr=curr->next;
-            }
-            else {
-                curr->next=l2;
-                l2=l2->next;
-                curr=curr->next;
-            }
-        }
-        if (l1!=NULL){
-            curr->next=l1;
-        }
-        if (l2!=NULL){
-            curr->next=l2;
-        }
-        return dummy->next;
+    // ListNode* helper(ListNode* l1,ListNode* l2){
+    //     ListNode * curr= new ListNode (-1);
+    //     ListNode* dummy =curr;
+    //     while(l1!=NULL && l2!=NULL){
+    //         if (l1->val<l2->val){
+    //             curr->next=l1;
+    //             l1=l1->next;
+    //             curr=curr->next;
+    //         }
+    //         else {
+    //             curr->next=l2;
+    //             l2=l2->next;
+    //             curr=curr->next;
+    //         }
+    //     }
+    //     if (l1!=NULL){
+    //         curr->next=l1;
+    //     }
+    //     if (l2!=NULL){
+    //         curr->next=l2;
+    //     }
+    //     return dummy->next;
     
-    }
+    // }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         auto cmp = [](ListNode* a, ListNode* b) { return a->val > b->val; };
         priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> pq(cmp);
